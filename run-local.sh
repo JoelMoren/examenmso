c5c5b251dd0229361fc37e272779550b954ffb1e
Joel Moreno Martinez <jjoerkk@gmail.com>
2023-01-09 19:46:01 -0600

    se modifica generacion de imagenes con tageo

--- run-local.sh
+++ run-local.sh
@@ -3,31 +3,37 @@
 
 cd msomonitoreo
 mvn clean package
+docker build -t joerk/msomonitoreo:1.0.0 .
 
 cd ..
 
 cd msoservicediscovery
-mvn clean package 
+mvn clean package
+docker build -t joerk/msoservicediscovery:1.0.0 .
 
 cd ..
 
 cd msousuarios
 mvn clean package
+docker build -t joerk/msousuarios:1.0.0 .
 
 cd ..
 
 cd msopublicaciones
 mvn clean package
+docker build -t joerk/msopublicaciones:1.0.0 .
 
 cd ..
 
 cd msomensajeria
 mvn clean package
+docker build -t joerk/msomensajeria:1.0.0 .
 
 cd ..
 
 cd front
 mvn clean package
+docker build -t joerk/msofront:1.0.0 .
 
 docker-compose up --build
 
